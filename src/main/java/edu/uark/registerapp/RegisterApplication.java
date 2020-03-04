26462a325187a8598dc992efaaa48d8118b81f05
Stephanie Phillips <60154372+srp001@users.noreply.github.com>
2020-03-03 23:15:25 -0600

    Update RegisterApplication.java

--- src/main/java/edu/uark/registerapp/RegisterApplication.java
+++ src/main/java/edu/uark/registerapp/RegisterApplication.java
@@ -2,9 +2,10 @@ package edu.uark.registerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("controllers")
+@ComponentScan({"controllers"})
 public class RegisterApplication {
 
 	public static void main(final String[] args) {
