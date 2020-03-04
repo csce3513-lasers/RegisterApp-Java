608588b2f1f0f46af52d1c00be8d707c1f7dcde7
Stephanie Phillips <60154372+srp001@users.noreply.github.com>
2020-03-03 21:55:10 -0600

    Update SignInRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,29 +1,29 @@
 package edu.uark.registerapp.controllers;
 
-import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
-import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
-import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
-import edu.uark.registerapp.models.api.EmployeeSignIn;
-
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.acls.model.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.controllers.BaseRouteController;
 
 @Controller
 @RequestMapping(value = "/")
+public class SignInRouteController extends BaseRouteController {
+	// TODO: Route for initial page load
 
-public class SignInRouteController {
-    //controller implented here   
     @RequestMapping(method = RequestMethod.GET)  
-    public ModelAndView signInGetRoute(@RequestParam Map<String, String> map) {
+    public ModelAndView showSignIn(@RequestParam Map<String, String> map) {
         ModelAndView modelAndView;
         try {
             //if employee exists, serve this page
@@ -36,21 +36,21 @@ public class SignInRouteController {
         return modelAndView;
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ModelAndView signInPostRoute(EmployeeSignIn employeeSignIn, HttpServletRequest req) {
-        ModelAndView modelAndView;
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ModelAndView performSignIn(EmployeeSignIn employeeSignIn, HttpServletRequest request) {
+
         employeeSignIn.setEmployeeId(req.getParameter("empID"));
         employeeSignIn.setPassword(req.getParameter("passWord"));
-        HttpSession session = req.getSession();
-        String sessionId = session.getId();
+		
         try {
-            EmployeeSignInCommand employeeSignInCommand = new EmployeeSignInCommand(employeeSignIn, sessionId);
-            modelAndView = new ModelAndView("redirect:/mainMenu"); 
+            EmployeeSignInCommand employeeSignInCommand = new EmployeeSignInCommand(employeeSignIn, request.getSession().getId());
+            return new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.MAIN_MENU.getRoute()));
         }
         catch(UnprocessableEntityException e){ //could not catch another NotFoundException
-            modelAndView = new ModelAndView("signInView");
+            ModelAndView modelAndView = new ModelAndView("signInView");
             modelAndView.addObject("errorMessage", "Sign-in unsuccessful. Please try again.");
+			return modelAndView;
         }
-        return modelAndView;
-    }
+		
+	}
 }
