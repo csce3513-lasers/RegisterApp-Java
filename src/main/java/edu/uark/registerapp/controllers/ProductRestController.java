48a8f60196fb52921a5b7a64da5965b25cd932fc
Stephanie Phillips <60154372+srp001@users.noreply.github.com>
2020-03-03 20:59:03 -0600

    Update ProductRestController.java

--- src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
@@ -1,10 +1,9 @@
 package edu.uark.registerapp.controllers;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.UUID;
-
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,70 +16,100 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.uark.registerapp.commands.products.ProductCreateCommand;
 import edu.uark.registerapp.commands.products.ProductDeleteCommand;
 import edu.uark.registerapp.commands.products.ProductUpdateCommand;
-import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
 import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 
 @RestController
 @RequestMapping(value = "/api/product")
-public class ProductRestController extends BaseRestController {
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public @ResponseBody ApiResponse createProduct(
-		@RequestBody final Product product,
-		final HttpServletRequest request,
-		final HttpServletResponse response
-	) {
+public class ProductRestController {
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public @ResponseBody ApiResponse createProduct(@RequestBody final Product product, HttpServletRequest req) {
+            
+        HttpSession session = req.getSession();
+        String sessionId = session.getId();
+        final Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionId);
+            
+        if(activeUserEntity.isPresent()) {
+            int classification = activeUserEntity.get().getClassification();
+            if(classification == 501 || classification == 701) {
+                return this.productCreateCommand.setApiProduct(product).execute();
+            }
+            else {
+                ApiResponse apiResponse = new ApiResponse();
+                apiResponse.setRedirectUrl("/productListing.html"); 
+                return apiResponse;
+            }
+        }
+        else {
+            ApiResponse apiResponse = new ApiResponse();
+            apiResponse.setRedirectUrl("/productListing.html"); 
+            return apiResponse;
+        }
+    }
 
-		final ApiResponse elevatedUserResponse =
-			this.redirectUserNotElevated(
-				request,
-				response,
-				ViewNames.PRODUCT_LISTING.getRoute());
+    @RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
+    public @ResponseBody ApiResponse updateProduct(@PathVariable final UUID productId, @RequestBody final Product product, HttpServletRequest req) {
+            
+        HttpSession session = req.getSession();
+        String sessionId = session.getId();
+        final Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionId);
+            
+        if(activeUserEntity.isPresent()) {
+            int classification = activeUserEntity.get().getClassification();
+            if(classification == 501 || classification == 701) {
+                return this.productUpdateCommand.setProductId(productId).setApiProduct(product).execute();
+            }
+            else {
+                ApiResponse apiResponse = new ApiResponse();
+                apiResponse.setRedirectUrl("/productListing.html"); 
+                return apiResponse;
+            }
+        }
+        else {
+            ApiResponse apiResponse = new ApiResponse();
+            apiResponse.setRedirectUrl("/productListing.html"); 
+            return apiResponse;
+        }
+    }
 
-		if (!elevatedUserResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
-			return elevatedUserResponse;
-		}
+    @RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
+    public @ResponseBody ApiResponse deleteProduct(@PathVariable final UUID productId, HttpServletRequest req) {
+            
+        HttpSession session = req.getSession();
+        String sessionId = session.getId();
+        final Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionId);
+          
+        if(activeUserEntity.isPresent()) {
+            int classification = activeUserEntity.get().getClassification();
+            if(classification == 501 || classification == 701) {
+                this.productDeleteCommand.setProductId(productId).execute();
+                return new ApiResponse();
+            }
+            else {
+                ApiResponse apiResponse = new ApiResponse();
+                apiResponse.setRedirectUrl("/productListing.html"); 
+                return apiResponse;
+            }
+        }
+        else {
+            ApiResponse apiResponse = new ApiResponse();
+            apiResponse.setRedirectUrl("/productListing.html"); 
+            return apiResponse;
+        }
+    }
 
-		return this.productCreateCommand
-			.setApiProduct(product)
-			.execute();
-	}
+    // Properties
+    @Autowired
+    private ProductCreateCommand productCreateCommand;
 
-	@RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
-	public @ResponseBody ApiResponse updateProduct(
-		@PathVariable final UUID productId,
-		@RequestBody final Product product
-	) {
+    @Autowired
+    private ProductDeleteCommand productDeleteCommand;
 
-		// TODO: Verify that the user associated with the current session is elevated
-
-		return this.productUpdateCommand
-			.setProductId(productId)
-			.setApiProduct(product)
-			.execute();
-	}
-
-	@RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
-	public @ResponseBody ApiResponse deleteProduct(
-		@PathVariable final UUID productId
-	) {
-
-		// TODO: Verify that the user associated with the current session is elevated
-
-		this.productDeleteCommand
-			.setProductId(productId)
-			.execute();
-
-		return new ApiResponse();
-	}
-
-	// Properties
-	@Autowired
-	private ProductCreateCommand productCreateCommand;
-	
-	@Autowired
-	private ProductDeleteCommand productDeleteCommand;
-	
-	@Autowired
-	private ProductUpdateCommand productUpdateCommand;
+    @Autowired
+    private ProductUpdateCommand productUpdateCommand;
+        
+    @Autowired
+    ActiveUserRepository activeUserRepository;
 }
