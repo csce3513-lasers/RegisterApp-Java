322fd706681721c148ebc46eb1d9f99ebc763281
Stephanie Phillips <60154372+srp001@users.noreply.github.com>
2020-03-03 20:26:10 -0600

    Update ProductDetailRouteController.java

--- ProductDetailRouteController.java
+++ ProductDetailRouteController.java
@@ -1,56 +1,93 @@
-package controllers;
+package edu.uark.registerapp.controllers;
 
-import java.util.Map;
+import java.util.UUID;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+
 @Controller
-@RequestMapping(value = "/productdetail.html")
+@RequestMapping(value = "/productDetail") //correct routename?
 
 public class ProductDetailRouteController {
-    @RequestMapping(method = RequestMethod.GET)  
-    public ModelAndView productDetailGetRoute(@RequestParam Map<String, String> map) {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView start(HttpServletRequest req) {
         ModelAndView modelAndView;
-        Boolean elevatedEmployee = false; // placeholder
-        if(elevatedEmployee) {
-            modelAndView = new ModelAndView("productDetailView");
-            modelAndView.addObject("disabledField", !elevatedEmployee);
-            modelAndView.addObject("elevatedUser", elevatedEmployee);
+        
+        HttpSession session = req.getSession();
+        String sessionId = session.getId();
+        final Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionId);
+        
+        if(activeUserEntity.isPresent()) {
+            int classification = activeUserEntity.get().getClassification();
+            if(classification == 501 || classification == 701) {
+                modelAndView = new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
+                modelAndView.addObject("disabledField", false); //defaults in view to true
+                modelAndView.addObject("elevatedUser", true); //defaults in view to false
+                modelAndView.addObject(ViewModelNames.PRODUCT.getValue(), (new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+            }
+            else {
+                modelAndView = new ModelAndView("redirect:/productListing.html"); 
+            }
         }
         else {
-            modelAndView = new ModelAndView("redirect:/productlisting.html");
+            modelAndView = new ModelAndView("redirect:/productListing.html"); 
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/productListing.html/{productId}", method = RequestMethod.GET) 
+    public ModelAndView startWithProduct(@PathVariable final UUID productId, HttpServletRequest req) {
+        ModelAndView modelAndView;
+        
+        HttpSession session = req.getSession();
+        String sessionId = session.getId();
+        final Optional<ActiveUserEntity> activeUserEntity = activeUserRepository.findBySessionKey(sessionId);
+        
+        if(activeUserEntity.isPresent()) {
+            int classification = activeUserEntity.get().getClassification();
+            if(classification == 501 || classification == 701) {
+                modelAndView = new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
+                modelAndView.addObject("disabledField", false); //defaults in view to true
+                modelAndView.addObject("elevatedUser", true); //defaults in view to false
+                try {
+                    modelAndView.addObject(ViewModelNames.PRODUCT.getValue(), this.productQuery.setProductId(productId).execute());
+                } catch (final Exception e) {
+                    modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), e.getMessage());
+                    modelAndView.addObject(ViewModelNames.PRODUCT.getValue(), (new Product()).setCount(0).setLookupCode(StringUtils.EMPTY));
+                }
+            }
+            else {
+                modelAndView = new ModelAndView("redirect:/productListing.html"); 
+            }
+        }
+        else {
+            modelAndView = new ModelAndView("redirect:/productListing.html");
         }
         return modelAndView;
     }
     
-    @RequestMapping(value = "/api/saveproduct", method = RequestMethod.POST)  
-    public ModelAndView productsaveRoute(@RequestParam Map<String, String> map) {
-        ModelAndView modelAndView;
-        Boolean elevatedEmployee = true; // placeholder
-        if(!elevatedEmployee) {
-            modelAndView = new ModelAndView("redirect:/productlisting.html");
-        }
-        else {
-            // save the product
-            modelAndView = new ModelAndView();
-        }
-        return modelAndView;
-    }
+
     
-    @RequestMapping(value = "/api/deleteproduct", method = RequestMethod.POST)  
-    public ModelAndView productdeleteRoute(@RequestParam Map<String, String> map) {
-        ModelAndView modelAndView;
-        Boolean elevatedEmployee = true; // placeholder
-        if(!elevatedEmployee) {
-            modelAndView = new ModelAndView("redirect:/productlisting.html");
-        }
-        else {
-            // delete the product 
-            modelAndView = new ModelAndView();
-        }
-        return modelAndView;
-    }
+    // Properties
+    @Autowired
+    private ProductQuery productQuery;
+    
+    @Autowired
+    ActiveUserRepository activeUserRepository;
 }
