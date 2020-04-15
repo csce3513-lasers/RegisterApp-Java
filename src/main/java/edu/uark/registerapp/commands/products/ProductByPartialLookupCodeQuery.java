cdd1883651a5b905f411bc25ad6935bf492b66fd
odessamarahelie <46230255+odessamarahelie@users.noreply.github.com>
2020-04-14 22:05:28 -0500

    added import statement

--- src/main/java/edu/uark/registerapp/commands/products/ProductByPartialLookupCodeQuery.java
+++ src/main/java/edu/uark/registerapp/commands/products/ProductByPartialLookupCodeQuery.java
@@ -1,6 +1,6 @@
 package edu.uark.registerapp.commands.products;
 
-//import java.util.Optional;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
