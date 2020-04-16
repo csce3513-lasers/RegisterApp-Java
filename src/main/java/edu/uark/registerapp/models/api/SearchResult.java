5cbbfcc50b54ef8679bce74a3796ff07a2599cfa
Stephanie <Stephanie@DESKTOP-GQTODU1>
2020-04-16 01:44:38 -0500

    Added lookupcode variable and methods to product class

--- src/main/java/edu/uark/registerapp/models/api/SearchResult.java
+++ src/main/java/edu/uark/registerapp/models/api/SearchResult.java
@@ -3,16 +3,23 @@ package edu.uark.registerapp.models.api;
 
 public class SearchResult {
     String productID;
+    String productLookUpCode;
     double productPrice;
 
     public String getProductID() {
         return productID;
+    }
+    public String getproductLookUpCode() {
+        return productLookUpCode;
     }
     public double getProductPrice() {
         return productPrice;
     }
     public void setProductID(String string) {
         this.productID = string;
+    }
+    public void setproductLookUpCode(String string) {
+        this.productLookUpCode = string;
     }
     public void setProductPrice(double productPrice) {
         this.productPrice = productPrice;
