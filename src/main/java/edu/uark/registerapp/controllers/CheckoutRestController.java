c97bf937eaa61cb65755282285c7dfd304df165f
ZachariahMcc <59979813+ZachariahMcc@users.noreply.github.com>
2020-04-28 13:17:35 -0500

    Update CheckoutRestController.java

--- src/main/java/edu/uark/registerapp/controllers/CheckoutRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/CheckoutRestController.java
@@ -121,7 +121,7 @@ public class CheckoutRestController extends BaseRestController {
         for(CartItem item : cart) {
             System.out.println("Product ID: " + item.getProductID() + " Quantity: " + item.getProductQuantity());
         }
-        
+        /*
         long totalPrice = cart.get(cart.size()-1).getTotalPrice(); // total price is stored on the last index
         cart.remove(cart.size()-1);  
 	 
@@ -138,6 +138,7 @@ public class CheckoutRestController extends BaseRestController {
         //WHAT GETS RETURNED
         List<CartItem> searchResults = new ArrayList<CartItem>();
                 //TRY TO CREATE ORDER
+                */
         UUID orderID = UUID.randomUUID(); //RANDOM UUID FOR TESTING PURPOSES
         
         //WHAT GETS RETURNED
